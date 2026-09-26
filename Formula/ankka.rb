da0eaa121ec6e8e4733f7f9db062121c8b8c5b39
ankka release <release@ankka.invalid>
2026-09-26 16:14:18 +0000

    ankka 0.6.4

--- Formula/ankka.rb
+++ Formula/ankka.rb
@@ -7,8 +7,8 @@
 class Ankka < Formula
   desc "Command-line client for ankka, a serverless platform for agentic AI"
   homepage "https://docs.ankka.cloud/"
-  url "https://github.com/thinkmorestupidless/ankka/releases/download/v0.0.0/ankka-cli-0.0.0.zip"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/thinkmorestupidless/ankka/releases/download/v0.6.4/ankka-cli-0.6.4.zip"
+  sha256 "eecd1190d5633918f7e52fff0fa916121de1ff2eb3a6d989cc478620725f8baf"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
